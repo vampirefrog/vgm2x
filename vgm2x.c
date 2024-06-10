366582d4b36c65b6ac5ed11c1f8aee5d6deec128
vampi <vampi@vampi.tech>
2024-06-10 06:02:26 +0300

    vgm2x working recursively (needs testing)

--- vgm2x.c
+++ vgm2x.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <libgen.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #ifdef HAVE_LIBARCHIVE
 #include <archive.h>
 #include <archive_entry.h>
@@ -14,13 +15,22 @@
 #include "cmdline.h"
 #include "tools.h"
 #include "vgm_analyzer.h"
+#include "opl_voice_collector.h"
 #include "opm_voice_collector.h"
+#include "opn_voice_collector.h"
+#include "opl_analyzer.h"
 #include "opm_analyzer.h"
 #include "opn_analyzer.h"
 #include "libvgm/utils/FileLoader.h"
 #include "libvgm/utils/MemoryLoader.h"
+#include "midilib/midi_file.h"
 
-#include <sys/stat.h>
+char *opt_output = "-";
+enum fm_voice_file_format opt_opl_format;
+enum fm_voice_file_format opt_opm_format;
+enum fm_voice_file_format opt_opn_format;
+int opt_csv = 0;
+
 int each_file(const char *path, int (*process_file)(const char *, void *), void *data_ptr) {
 	struct stat st;
 	int r = stat(path, &st);
@@ -54,10 +64,177 @@ int each_file(const char *path, int (*process_file)(const char *, void *), void 
 	return 0;
 }
 
-static int vgm_file_cb(DATA_LOADER *loader, char *filename_base, char *target_dir, void *data_ptr) {
-	printf("vgm_file_cb %s/%s.opm\n", filename_base, target_dir);
+size_t write_fn(void *buf, size_t bufsize, void *data_ptr) {
+	return fwrite(buf, 1, bufsize, (FILE *)data_ptr);
+}
+
+static int vgm_file_cb(DATA_LOADER *loader, char *target_dir, char *filename_base, void *data_ptr) {
+	struct vgm_analyzer va;
+	vgm_analyzer_init(&va);
+
 	DataLoader_ReadAll(loader);
-	return vgm_analyzer_run(data_ptr, DataLoader_GetData(loader), DataLoader_GetSize(loader));
+	int r = vgm_analyzer_run(&va, DataLoader_GetData(loader), DataLoader_GetSize(loader));
+	if(r) return r;
+
+	struct opl_voice_collector opl_collector;
+	opl_voice_collector_init(&opl_collector);
+	struct opm_voice_collector opm_collector;
+	opm_voice_collector_init(&opm_collector);
+	struct opn_voice_collector opn_collector;
+	opn_voice_collector_init(&opn_collector);
+	enum vgm_chip_id ids[] = {
+		YM3526, SECOND_YM3526, // OPL
+		YM3812, SECOND_YM3812, // OPL2
+		YM2151, SECOND_YM2151, // OPM
+		YM2203, SECOND_YM2203, // OPN
+		YM2608, SECOND_YM2608, // OPNA
+		YM2610, SECOND_YM2610, // OPNB
+		YM2612, SECOND_YM2612, // OPN2
+	};
+	for(int i = 0; i < sizeof(ids) / sizeof(ids[0]); i++) {
+		if(!va.analyzers_by_id[ids[i]]) continue;
+		if( // OPLx
+			ids[i] == YM3526 || ids[i] == SECOND_YM3526 ||
+			ids[i] == YM3812 || ids[i] == SECOND_YM3812
+		) {
+			struct opl_analyzer *a = (struct opl_analyzer *)va.analyzers_by_id[ids[i]];
+			for(int i = 0; i < a->collector.num_voices; i++) {
+				struct opl_voice_collector_voice oplv;
+				memcpy(&oplv, a->collector.voices + i, sizeof(oplv));
+				opl_voice_collector_push_voice(&opl_collector, &oplv, -1, -1);
+			}
+		} else if( // OPM
+			ids[i] == YM2151 || ids[i] == SECOND_YM2151
+		) {
+			struct opm_analyzer *a = (struct opm_analyzer *)va.analyzers_by_id[ids[i]];
+			for(int i = 0; i < a->collector.num_voices; i++) {
+				struct opm_voice_collector_voice opmv;
+				memcpy(&opmv, a->collector.voices + i, sizeof(opmv));
+				opm_voice_collector_push_voice(&opm_collector, &opmv, -1, -1);
+			}
+		} else if( // OPN
+			ids[i] == YM2612 || ids[i] == SECOND_YM2612 ||
+			ids[i] == YM2203 || ids[i] == SECOND_YM2203 ||
+			ids[i] == YM2608 || ids[i] == SECOND_YM2608 ||
+			ids[i] == YM2610 || ids[i] == SECOND_YM2610
+		) {
+			struct opn_analyzer *a = (struct opn_analyzer *)va.analyzers_by_id[ids[i]];
+			for(int i = 0; i < a->collector.num_voices; i++) {
+				struct opn_voice_collector_voice opnv;
+				memcpy(&opnv, a->collector.voices + i, sizeof(opnv));
+				opn_voice_collector_push_voice(&opn_collector, &opnv, -1, -1);
+			}
+		}
+	}
+
+	struct fm_voice_bank bank;
+	fm_voice_bank_init(&bank);
+	for(int i = 0; i < opl_collector.num_voices; i++)
+		fm_voice_bank_append_opl_voice(&bank, &opl_collector.voices[i].voice);
+	for(int i = 0; i < opm_collector.num_voices; i++)
+		fm_voice_bank_append_opm_voice(&bank, &opm_collector.voices[i].voice);
+	for(int i = 0; i < opn_collector.num_voices; i++)
+		fm_voice_bank_append_opn_voice(&bank, &opn_collector.voices[i].voice);
+
+	char fmfile[PATH_MAX];
+	if(bank.num_opl_voices > 0) {
+		snprintf(fmfile, sizeof(fmfile), "%s/%s.%s", target_dir, filename_base, fm_get_voice_file_format_extension(opt_opl_format));
+		FILE *o = fopen(fmfile, "w");
+		if(!o) {
+			fprintf(stderr, "Could not open %s: %s (%d)\n", fmfile, strerror(errno), errno);
+			return -1;
+		} else {
+			fm_voice_bank_save(&bank, opt_opl_format, write_fn, o);
+			fclose(o);
+		}
+	}
+	if(bank.num_opm_voices > 0) {
+		snprintf(fmfile, sizeof(fmfile), "%s/%s.%s", target_dir, filename_base, fm_get_voice_file_format_extension(opt_opm_format));
+		FILE *o = fopen(fmfile, "w");
+		if(!o) {
+			fprintf(stderr, "Could not open %s: %s (%d)\n", fmfile, strerror(errno), errno);
+			return -1;
+		} else {
+			fm_voice_bank_save(&bank, opt_opm_format, write_fn, o);
+			fclose(o);
+		}
+	}
+	if(bank.num_opn_voices > 0) {
+		snprintf(fmfile, sizeof(fmfile), "%s/%s.%s", target_dir, filename_base, fm_get_voice_file_format_extension(opt_opn_format));
+		FILE *o = fopen(fmfile, "w");
+		if(!o) {
+			fprintf(stderr, "Could not open %s: %s (%d)\n", fmfile, strerror(errno), errno);
+			return -1;
+		} else {
+			fm_voice_bank_save(&bank, opt_opn_format, write_fn, o);
+			fclose(o);
+		}
+	}
+
+	int total_tracks = 0;
+	for(int i = 0; i < va.num_chip_analyzers; i++)
+		total_tracks += va.analyzers[i]->num_tracks;
+	struct midi_file m;
+	midi_file_init(&m, MIDI_FORMAT_MULTI_TRACKS, total_tracks, 44100*120/60);
+	int cur_track = 0;
+	for(int i = 0; i < va.num_chip_analyzers; i++) {
+		for(int j = 0; j < va.analyzers[i]->num_tracks; j++) {
+			memcpy(m.tracks + cur_track, &va.analyzers[i]->tracks[j].midi_track, sizeof(m.tracks[0]));
+			cur_track++;
+		}
+	}
+
+	char midifilename[512];
+	snprintf(midifilename, sizeof(midifilename), "%s/%s.mid", target_dir, filename_base);
+	FILE *f = fopen(midifilename, "wb");
+	if(!f) {
+		fprintf(stderr, "Could not open poo.mid: %s (%d)\n", strerror(errno), errno);
+		return -2;
+	}
+	midi_file_write(&m, write_fn, f);
+	fclose(f);
+
+	// if(opt_csv) {
+	// 	printf("FB\tCON\tPMS\tAMS\tSLOT");
+	// 	for(int i = 0; i < 4; i++) {
+	// 		printf("\tAR\tD1R\tD2R\tRR\tD1L\tTL\tKS\tMUL\tDT1\tDT2\tAMS-EN");
+	// 	}
+	// 	printf("\n");
+	// 	for(int i = 0; i < collector.num_voices; i++) {
+	// 		struct opm_voice_collector_voice *v = collector.voices + i;
+	// 		printf(
+	// 			"%d\t%d\t%d\t%d\t%d",
+	// 			v->voice.rl_fb_con >> 3 & 0x07,
+	// 			v->voice.rl_fb_con & 0x07,
+	// 			v->voice.pms_ams >> 4 & 0x07,
+	// 			v->voice.pms_ams & 0x03,
+	// 			v->voice.slot
+	// 		);
+	// 		for(int j = 0; j < 4; j++) {
+	// 			const uint8_t dtmap[] = { 3, 4, 5, 6,  3, 2, 1, 0 };
+	// 			struct opm_voice_operator *op = &v->voice.operators[j];
+	// 			printf(
+	// 				"\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
+	// 				op->ks_ar & 0x1f,
+	// 				op->ams_d1r & 0x1f,
+	// 				op->dt2_d2r & 0x1f,
+	// 				op->d1l_rr & 0x0f,
+	// 				op->d1l_rr >> 4,
+	// 				op->tl & 0x7f,
+	// 				op->ks_ar >> 6,
+	// 				op->dt1_mul & 0x0f,
+	// 				dtmap[op->dt1_mul >> 4 & 0x07],
+	// 				op->dt2_d2r >> 6,
+	// 				op->ams_d1r >> 7
+	// 			);
+	// 		}
+	// 		printf("\n");
+	// 	}
+
+	// 	return 0;
+	// }
+
+	return 0;
 }
 
 struct vgm_file_cb_pair {
@@ -97,6 +274,15 @@ static int each_vgm_file_cb(const char *filename, void *data_ptr) {
 			zip_close(z);
 			return -1;
 		}
+		char writable_path[PATH_MAX];
+		strncpy(writable_path, filename, sizeof(writable_path));
+		if(writable_path[l-4] == '.')
+			writable_path[l-4] = 0;
+		mkdir(writable_path, 0777);
+		if(errno && errno != EEXIST) {
+			fprintf(stderr, "Could not make directory %s: %s (%d)\n", writable_path, strerror(errno), errno);
+			return errno;
+		}
 		for(int j = 0; j < num_entries; j++) {
 			zip_stat_t st;
 			zip_stat_index(z, j, ZIP_STAT_NAME | ZIP_STAT_SIZE, &st);
@@ -129,16 +315,12 @@ static int each_vgm_file_cb(const char *filename, void *data_ptr) {
 					DataLoader_Deinit(dload);
 					return -1;
 				}
-				char writable_path[PATH_MAX];
 				char base[PATH_MAX];
-				strncpy(writable_path, filename, sizeof(writable_path));
-				if(writable_path[l-4] == '.')
-					writable_path[l-4] = 0;
 				strncpy(base, st.name, sizeof(base));
 				char *b = basename(base);
 				char *dot = strrchr(b, '.');
 				if(dot) *dot = 0;
-				int r = pair->process_file(dload, basename(writable_path), b, pair->data_ptr);
+				int r = pair->process_file(dload, writable_path, b, pair->data_ptr);
 				DataLoader_Deinit(dload);
 				if(r) return r;
 			}
@@ -178,13 +360,10 @@ int each_vgm_file(const char *path, int (*process_file)(DATA_LOADER *, char *, c
 	return each_file(path, each_vgm_file_cb, &pair);
 }
 
-size_t write_fn(void *buf, size_t bufsize, void *data_ptr) {
-	return fwrite(buf, 1, bufsize, (FILE *)data_ptr);
-}
-
-char *opt_output = "-";
-int opt_csv = 0;
 int main(int argc, char **argv) {
+	char *opt_opl_format_str = "BNK";
+	char *opt_opm_format_str = "OPM";
+	char *opt_opn_format_str = "INS";
 	int optind = cmdline_parse_args(argc, argv, (struct cmdline_option[]){
 		{
 			'o', "output",
@@ -192,6 +371,27 @@ int main(int argc, char **argv) {
 			"name",
 			TYPE_OPTIONAL,
 			TYPE_STRING, &opt_output
+		},
+		{
+			'l', "opl-format",
+			"Output format for OPL voices: SBI, INS, BNK, IBK, CMF",
+			"format",
+			TYPE_OPTIONAL,
+			TYPE_STRING, &opt_opl_format_str
+		},
+		{
+			'm', "opm-format",
+			"Output format for OPM voices: OPM, SYX_FB01, SYX_DX21",
+			"format",
+			TYPE_OPTIONAL,
+			TYPE_STRING, &opt_opm_format_str
+		},
+		{
+			'n', "opn-format",
+			"Output format for OPN voices: INS, DMP, TFI, Y12",
+			"format",
+			TYPE_OPTIONAL,
+			TYPE_STRING, &opt_opn_format_str
 		},
 		{
 			'c', "csv",
@@ -206,99 +406,14 @@ int main(int argc, char **argv) {
 
 	if(optind < 0) exit(-optind);
 
-	struct vgm_analyzer va;
-	vgm_analyzer_init(&va);
+	opt_opl_format = fm_get_voice_file_format_from_name(opt_opl_format_str);
+	opt_opm_format = fm_get_voice_file_format_from_name(opt_opm_format_str);
+	opt_opn_format = fm_get_voice_file_format_from_name(opt_opn_format_str);
 
 	for(int i = optind; i < argc; i++) {
-		int r = each_vgm_file(argv[i], vgm_file_cb, &va);
-		if(r) fprintf(stderr, "Could not proces %s: %d\n", argv[i], r);
+		int r = each_vgm_file(argv[i], vgm_file_cb, 0);
+		if(r) fprintf(stderr, "Could not process %s: %d\n", argv[i], r);
 	}
-
-	struct opm_voice_collector collector;
-	opm_voice_collector_init(&collector);
-	enum vgm_chip_id ids[] = {
-		YM2151, SECOND_YM2151,
-		YM2612, SECOND_YM2612,
-		YM2203, SECOND_YM2203,
-		YM2608, SECOND_YM2608,
-		YM2610, SECOND_YM2610,
-		YM3812, SECOND_YM3812,
-		YM3526, SECOND_YM3526,
-	};
-	for(int i = 0; i < sizeof(ids) / sizeof(ids[0]); i++) {
-		if(!va.analyzers_by_id[ids[i]]) continue;
-		if(ids[i] == YM2151 || ids[i] == SECOND_YM2151) {
-			struct opm_analyzer *a = (struct opm_analyzer *)va.analyzers_by_id[ids[i]];
-			for(int i = 0; i < a->collector.num_voices; i++) {
-				struct opm_voice_collector_voice opmv;
-				memcpy(&opmv, a->collector.voices + i, sizeof(opmv));
-				opm_voice_collector_push_voice(&collector, &opmv, -1, -1);
-			}
-		} else {
-			struct opn_analyzer *a = (struct opn_analyzer *)va.analyzers_by_id[ids[i]];
-			for(int i = 0; i < a->collector.num_voices; i++) {
-				struct opn_voice_collector_voice *opnv = a->collector.voices + i;
-				struct opm_voice_collector_voice opmv;
-				opmv.vgm_ofs = opnv->vgm_ofs;
-				opmv.chan_used_mask = opnv->chan_used_mask;
-				memcpy(opmv.note_usage, opnv->note_usage, sizeof(opnv->note_usage));
-				opm_voice_load_opn_voice(&opmv.voice, &opnv->voice);
-				opm_voice_collector_push_voice(&collector, &opmv, -1, -1);
-			}
-		}
-	}
-
-	if(opt_csv) {
-		printf("FB\tCON\tPMS\tAMS\tSLOT");
-		for(int i = 0; i < 4; i++) {
-			printf("\tAR\tD1R\tD2R\tRR\tD1L\tTL\tKS\tMUL\tDT1\tDT2\tAMS-EN");
-		}
-		printf("\n");
-		for(int i = 0; i < collector.num_voices; i++) {
-			struct opm_voice_collector_voice *v = collector.voices + i;
-			printf(
-				"%d\t%d\t%d\t%d\t%d",
-				v->voice.rl_fb_con >> 3 & 0x07,
-				v->voice.rl_fb_con & 0x07,
-				v->voice.pms_ams >> 4 & 0x07,
-				v->voice.pms_ams & 0x03,
-				v->voice.slot
-			);
-			for(int j = 0; j < 4; j++) {
-				const uint8_t dtmap[] = { 3, 4, 5, 6,  3, 2, 1, 0 };
-				struct opm_voice_operator *op = &v->voice.operators[j];
-				printf(
-					"\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
-					op->ks_ar & 0x1f,
-					op->ams_d1r & 0x1f,
-					op->dt2_d2r & 0x1f,
-					op->d1l_rr & 0x0f,
-					op->d1l_rr >> 4,
-					op->tl & 0x7f,
-					op->ks_ar >> 6,
-					op->dt1_mul & 0x0f,
-					dtmap[op->dt1_mul >> 4 & 0x07],
-					op->dt2_d2r >> 6,
-					op->ams_d1r >> 7
-				);
-			}
-			printf("\n");
-		}
-
-		return 0;
-	}
-
-	struct fm_voice_bank bank;
-	fm_voice_bank_init(&bank);
-	for(int i = 0; i < collector.num_voices; i++) {
-		struct opm_voice_collector_voice *v = collector.voices + i;
-		fm_voice_bank_append_opm_voice(&bank, &v->voice);
-	}
-
-	int is_stdout = opt_output && opt_output[0] == '-' && opt_output[1] == 0;
-	FILE *o = is_stdout ? stdout : fopen(opt_output, "w");
-	fm_voice_bank_save(&bank, FORMAT_OPM, write_fn, o);
-	if(!is_stdout) fclose(o);
 
 	return 0;
 }
